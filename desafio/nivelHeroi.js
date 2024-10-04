1ed559295ad7fd567fe144aa685d5e6bbc1659e4
cadu <cadubrasil.com@gmail.com>
2024-10-04 17:33:54 -0300

    mudando a variável status para ranking

--- desafio/nivelHeroi.js
+++ desafio/nivelHeroi.js
@@ -1,29 +1,29 @@
 let heroi = "link";
 let xp =  Math.floor(Math.random() * 15000);
-let status = "";
+let ranking = "";
 
 if (xp < 1000) {
-    status = "ferro"
-    console.log("o heroi de nome " + heroi + " esta com o xp de " + xp + " com o ranking de " + status);
+ ranking = "ferro"
+    console.log("o heroi de nome " + heroi + " esta com o xp de " + xp + " com o ranking de " + ranking);
 } else if (xp > 1001 && xp < 2000) {
-    status = "bronze";
-    console.log("o heroi de nome " + heroi + " esta com o xp de " + xp + " com o ranking de " + status);
+ ranking = "bronze";
+    console.log("o heroi de nome " + heroi + " esta com o xp de " + xp + " com o ranking de " + ranking);
 } else if (xp > 2001 && xp < 5000) {
-    status = "prata";
-    console.log("o heroi de nome " + heroi + " esta com o xp de " + xp + " com o ranking de " + status);
+ ranking = "prata";
+    console.log("o heroi de nome " + heroi + " esta com o xp de " + xp + " com o ranking de " + ranking);
 } else if (xp > 5001 && xp < 7000) {
-    status = "ouro";
-    console.log("o heroi de nome " + heroi + " esta com o xp de " + xp + " com o ranking de " + status);
+ ranking = "ouro";
+    console.log("o heroi de nome " + heroi + " esta com o xp de " + xp + " com o ranking de " + ranking);
 } else if (xp > 7001 && xp < 8000) {
-    status = "platina";
-    console.log("o heroi de nome " + heroi + " esta com o xp de " + xp + " com o ranking de " + status);
+ ranking = "platina";
+    console.log("o heroi de nome " + heroi + " esta com o xp de " + xp + " com o ranking de " + ranking);
 } else if (xp > 8001 && xp < 9000) {
-    status = "ascendente";
-    console.log("o heroi de nome " + heroi + " esta com o xp de " + xp + " com o ranking de " + status);
+ ranking = "ascendente";
+    console.log("o heroi de nome " + heroi + " esta com o xp de " + xp + " com o ranking de " + ranking);
 } else if (xp > 9001 && xp < 10000) {
-    status = "imortal";
-    console.log("o heroi de nome " + heroi + " esta com o xp de " + xp + " com o ranking de " + status);
+ ranking = "imortal";
+    console.log("o heroi de nome " + heroi + " esta com o xp de " + xp + " com o ranking de " + ranking);
 } else {
-    status = "radiante";
-    console.log("o heroi de nome " + heroi + " esta com o xp de " + xp + " com o ranking de " + status);
+ ranking = "radiante";
+    console.log("o heroi de nome " + heroi + " esta com o xp de " + xp + " com o ranking de " + ranking);
 };
